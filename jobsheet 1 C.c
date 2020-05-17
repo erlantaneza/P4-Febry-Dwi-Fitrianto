d14c5e8cdbdc21b3d80e31cf983558b34a4cc06b
erlantaneza <64582394+erlantaneza@users.noreply.github.com>
2020-05-16 23:05:47 -0700

    Create jobsheet 1 C.c
    Menampilkan array satu dimensi

---
+++ jobsheet 1 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main (){
+int nim[7]= {8,1,6,4,3,5,9};
+
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+printf ("Nim ke-6 = %i\n",nim[5]);
+printf ("Nim ke-7 = %i\n",nim[6]);
+
+}
