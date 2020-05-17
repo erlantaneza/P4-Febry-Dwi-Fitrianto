ffb0dd26b069c703a613548fc888c37e6ab74779
erlantaneza <64582394+erlantaneza@users.noreply.github.com>
2020-05-16 23:42:43 -0700

    Create jobsheet 2 C.c
    Mengisi dan Menampilkan Elemen Aray(Dinamis)

---
+++ jobsheet 2 C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+main (){
+	int x[7];
+	printf("masukan nilai 1:");
+	scanf("%i", &x[0]);
+	printf("masukan nilai 2:");
+	scanf("%i",&x[1]);
+	printf("masukan nilai 3:");
+	scanf("%i",&x[2]);
+	printf("masukan nilai 4:");
+	scanf("%i",&x[3]);
+	printf("masukan niali 5:");
+	scanf("%i",&x[4]);
+	printf("masukan nilai 6:");
+	scanf("%i",&x[5]);
+	printf("masukan nilai 7:");
+	scanf("%i",&x[6]);
+	printf("\n");
+	printf("nilai ke-1=%i\n",x[0]);
+	printf("nilai ke-2=%i\n",x[1]);
+	printf("nilai ke-3=%i\n",x[2]);
+	printf("nilai ke-4=%i\n",x[3]);
+	printf("nilai ke-5=%i\n",x[4]);
+	printf("nilai ke-6=%i\n",x[5]);
+	printf("nilai ke-7=%i\n",x[5]);
+	return 0;
+}
