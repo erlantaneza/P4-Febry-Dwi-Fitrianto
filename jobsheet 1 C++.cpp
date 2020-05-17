c8d279125a27ac93517f60443a2cdb2c2546d799
erlantaneza <64582394+erlantaneza@users.noreply.github.com>
2020-05-16 23:41:09 -0700

    Create jobsheet 1 C++.cpp
    Menampilkan array satu dimensi menggunakan bahasa C++

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"NAMA 	: ERLAN TANEZA\n\n";
+	cout<<"NIM  	: 7\n\n";
+	cout<<"KELPMPOK : 10\n\n";
+int nim[7]= {8,1,6,4,3,5,9};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+
+}
