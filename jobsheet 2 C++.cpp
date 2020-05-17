ba74d39656c0864d0a456a91e47d0dc413c9107f
erlantaneza <64582394+erlantaneza@users.noreply.github.com>
2020-05-16 23:44:33 -0700

    Create jobsheet 2 C++.cpp
    Mengisi dan Menampilkan Elemen Aray(Dinamis) menggunakan bahasa C++

---
+++ jobsheet 2 C++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x [7];
+cout<<"masukan nim 1 : ";
+cin>>x[0];
+cout<<"masukan nim 2 : ";
+cin>>x[1];
+cout<<"masukan nim 3 : ";
+cin>>x[2];
+cout<<"masukan nim 4 : ";
+cin>>x[3];
+cout<<"masukan nim 5 : ";
+cin>>x[4];
+cout<<"masukan nim 6 : ";
+cin>>x[5];
+cout<<"masukan nim 7 : ";
+cin>>x[6];
+cout<<endl;
+cout<<"nim ke-1="<<x[0]<<endl;
+cout<<"nim ke-2="<<x[1]<<endl;
+cout<<"nim ke-3="<<x[2]<<endl;
+cout<<"nim ke-4="<<x[3]<<endl;
+cout<<"nim ke-5="<<x[4]<<endl;
+cout<<"nim ke-6="<<x[5]<<endl;
+cout<<"nim ke-7="<<x[6]<<endl;
+system("pause");
+}
